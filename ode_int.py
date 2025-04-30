c86c882b2b085648a4f9b6e2ba66caf3b0dea068
ShioyaKaito <arm9lorderhax@gmail.com>
2025-04-30 15:53:34 +0900

    feat: Added leap frog method in ode_int

--- ode_int.py
+++ ode_int.py
@@ -12,6 +12,7 @@ class NumIntegral:
             "euler_method": self.euler_method,
             "modified_euler_method": self.modified_euler_method,
             "runge_kutta_method": self.runge_kutta_method,
+            "leap_frog_method": self.leap_frog_method,
         }
 
     def initialize(
@@ -22,6 +23,7 @@ class NumIntegral:
         step: float,
         algorithm: str = "euler_method",
         custom_algorithm: Optional[Callable[[np.ndarray], np.ndarray]] = None,
+        x_1: Optional[np.ndarray] = None,
     ) -> None:
         self.t_0 = t_0
         self.x_0 = x_0
@@ -34,9 +36,14 @@ class NumIntegral:
                 self.algorithm_dict[algorithm] = custom_algorithm
         self.step = step
         self.x = x_0
-        self.t = t_0
-        self._historical_t = [t_0]
-        self._historical_x = [x_0]
+        if x_1 is not None:
+            self.t = t_0 + self.step
+            self._historical_t = [t_0, t_0 + self.step]
+            self._historical_x = [x_0, x_1]
+        else:
+            self.t = t_0
+            self._historical_t = [t_0]
+            self._historical_x = [x_0]
 
     def run_integral(self, last_t: float, progress_bar: bool = True) -> None:
         t_range = [int(self.t_0 / self.step), int(last_t / self.step)]
@@ -51,7 +58,10 @@ class NumIntegral:
 
     def update(self, x: np.ndarray) -> None:
         self.t += self.step
-        self.x = self.x + self.algorithm_dict[self.update_algorithm](x)
+        if self.update_algorithm == "leap_frog_method":
+            self.x = self.historical_x[-2] + self.algorithm_dict[self.update_algorithm](x)
+        else:
+            self.x = self.x + self.algorithm_dict[self.update_algorithm](x)
         # 状態を記録
         self._historical_t.append(self.t)
         self._historical_x.append(self.x)
@@ -69,6 +79,9 @@ class NumIntegral:
         k_3 = self.x_dot(x + (k_2 * 0.5)) * self.step
         k_4 = self.x_dot(x + k_3) * self.step
         return (k_1 + (2 * k_2) + (2 * k_3) + k_4) / 6
+
+    def leap_frog_method(self, x: np.ndarray) -> np.ndarray:
+        return self.x_dot(x) * self.step * 2
 
     @property
     def historical_t(self) -> np.ndarray:
